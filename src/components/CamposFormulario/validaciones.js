b4ff150e95a4ad8a6373cf6def8d2100a89bec20
Waldo-analista <114253167+Waldo-analista@users.noreply.github.com>
2023-09-05 00:13:45 +0000

    Deploying to gh-pages from  @ 23836e067efaa6be6453815e2762364287467d0e 🚀

--- src/components/CamposFormulario/validaciones.js
+++
@@ -1,7 +0,0 @@
-export const validarCampoVacio = (value) => {
-  if (value.length === 0) {
-    return false;
-  } else {
-    return true;
-  }
-};
--- src/funcionesUtiles.js
+++
@@ -1,7 +0,0 @@
-export const firstLetterCapital = (text) => {
-  if (text.length < 5) {
-    return text.toUpperCase();
-  } else {
-    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
-  }
-};
